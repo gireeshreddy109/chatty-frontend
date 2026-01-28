6370c82944b5e05c2fea820126488d8273082eb5
gireeshreddy109 <ygireesh.reddy.eee21@itbhu.ac.in>
2026-01-28 08:05:03 +0530

    feat: Implement login form component and persistent authentication state management using Zustand.

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,31 +1,31 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 export type User = {
-    id: string,
-    userName: string,
-    fullName: string,
-    email: string,
-    connectCode: string,
+    id: string;
+    fullName: string;
+    username: string;
+    connectCode: string;
+    email: string;
 }
 
 interface AuthState {
-    user: User | null;
+    user: User | null,
     isAuthenticated: boolean;
-    setUser: (user: User | null) => void;
+    setUser: (user: User) => void;
     logout: () => void;
 }
 
-export const AuthStore = create<AuthState>()(
+export const useAuthStore = create<AuthState>()(
     persist(
         (set) => ({
             user: null,
             isAuthenticated: false,
-            setUser: (user: User | null) => set({ user }),
-            logout: () => set({ user: null, isAuthenticated: false }),
+            setUser: (user) => set({user, isAuthenticated: true}),
+            logout: () => set({user: null, isAuthenticated: false})
         }),
         {
-            name: "auth",
-        },
-    ),
+            name: "auth-storage",
+        }
+    )
 )
